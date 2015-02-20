a01d1812fdd45b3f8bd201b633a970e37f5b855a
unknown <sharma.susheel4@gmail.com>
2015-02-20 19:15:50 +0530

    commit all

---
+++ Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="color:red; height: 130px;">
+    
+        
+        <asp:textbox ID="Textbox1" runat="server" Width="152px"></asp:textbox>
+
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
+    
+        <br />
+    
+        
+        <asp:textbox ID="Textbox2" runat="server" Width="149px"></asp:textbox>
+
+    
+    </div>
+    </form>
+   
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+    SqlCommand cmd = new SqlCommand();
+     
+       
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        cmd = new SqlCommand("insert into username(name)values('"+Textbox1.Text+"')",sqlcon);
+        sqlcon.Open();
+
+        cmd.ExecuteNonQuery();
+
+        sqlcon.Close();
+
+        Textbox1.Text = "";
+
+        Response.Write("data saved...");
+
+    }
+}
